bb8ccbf967adfa9253b87681043ad66deb3f1a3d
Ivan Martins <ivanmartins.ilha@gmail.com>
2024-08-21 21:17:53 -0300

    feat: add component of list of friend

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,43 @@ const getMessageInfo = (balance) => balance < 0
  ? {message: `Te deve ${balance} reais`, color: 'text-green-500'}
  : {message: `Estamos quites ${balance} reais`, color: ""}
 
+const ListOfFriends = ({friend, selectedFriend, onClickFriend}) =>{
+  return(
+
+  <ul className=" w-full mt-10 ">
+    
+      { 
+      friend.map(friend => {
+        const {message, color} = getMessageInfo(friend.balance)
+        const isSelectedFriend = friend.id === selectedFriend?.id
+    
+        return (
+
+              <>
+                  <li className="border w-full rounded-md flex justify-between px-6 items-center mb-4">
+                    <div className="flex space-x-3 mr-46 ">
+                        <img src={friend.image} alt={friend.name} className="w-16 h-16 rounded-full"/>
+                      <div className="mr-30">
+                        <h2>{friend.name}</h2>
+                        <p className={color}>{message}</p>
+                      </div>
+                    </div>
+                    <div>
+                    <button onClick={() => onClickFriend(friend)} className={`inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10 button ${isSelectedFriend ? "bg-red-400": ""}`}>
+                        {isSelectedFriend ? "Fechar" : "Selecionar"}
+                      </button>
+                    </div>
+                  </li>
+              </>
+                )
+          }
+    
+      )
+    }
+      
+    </ul>
+  )
+}
 
 
 const App =() =>{
@@ -64,39 +101,12 @@ const App =() =>{
 
   return(
   <main className="flex flex-col">
-    <ul className=" w-full mt-10 ">
-    
-      { 
-      friend.map(friend => {
-        const {message, color} = getMessageInfo(friend.balance)
-        const isSelectedFriend = friend.id === selectedFriend?.id
-    
-        return (
-
-              <>
-                  <li className="border w-full rounded-md flex justify-between px-6 items-center mb-4">
-                    <div className="flex space-x-3 mr-46 ">
-                        <img src={friend.image} alt={friend.name} className="w-16 h-16 rounded-full"/>
-                      <div className="mr-30">
-                        <h2>{friend.name}</h2>
-                        <p className={color}>{message}</p>
-                      </div>
-                    </div>
-                    <div>
-                    <button onClick={() => handleClickFriend(friend)} className={`inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10 button ${isSelectedFriend ? "bg-red-400": ""}`}>
-                        {isSelectedFriend ? "Fechar" : "Selecionar"}
-                      </button>
-                    </div>
-                  </li>
-              </>
-                )
-          }
-    
-      )
-    }
-      
-    </ul>
-     <button  
+      <ListOfFriends 
+       friend={friend}
+       selectedFriend={selectedFriend}
+       onClickFriend={handleClickFriend}
+      />
+       <button  
           onClick={handleClickShowForm} 
           className={`mt-5 flex w-80 justify-center rounded-md  px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 m-auto ${showFormAddFriends ? "bg-gray-500" : "bg-indigo-500" }`}
         >
